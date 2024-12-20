37810f3f27db21eea97424824612997f264b4c98
Sebastian Haglund <sebastian.haglund@snowsoftware.com>
2024-12-20 10:55:45 +0100

    CDC-5573 - Example postinstall for linux least privileges and randomization

---
+++ linux/snow_post_install_linux_least_privileges.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# User to run agent as
+USER=root
+# cron schedule to run the agent, e.g 21:00 every day
+PERIODICITY='0 21 * * *'
+# Sleep up to 90 minutes before running the agent
+RANDOMIZATION=60
+
+/opt/snow/snowcron -u ${USER} -x ${RANDOMIZATION} "${PERIODICITY}" /opt/snow/snowagent
