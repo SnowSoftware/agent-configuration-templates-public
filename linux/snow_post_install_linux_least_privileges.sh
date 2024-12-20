5bd4cdc8662f4ba619b74c49395adc7ad9b0081f
Sebastian Haglund <sebastian@haglund.se>
2024-12-20 11:40:07 +0100

    CDC-5573 - Fix comment

--- linux/snow_post_install_linux_least_privileges.sh
+++ linux/snow_post_install_linux_least_privileges.sh
@@ -4,7 +4,7 @@
 USER=root
 # cron schedule to run the agent, e.g 21:00 every day
 PERIODICITY='0 21 * * *'
-# Sleep up to 90 minutes before running the agent
+# Sleep up to 60 minutes before running the agent
 RANDOMIZATION=60
 
 /opt/snow/snowcron -u ${USER} -x ${RANDOMIZATION} "${PERIODICITY}" /opt/snow/snowagent
